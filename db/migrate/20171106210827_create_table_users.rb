211212c79fc15e1686918ec2af3e7e9fca3c12cd
Emily Morgan <emorgan05@gmail.com>
2017-11-06 21:12:09 +0000

    automatically backed up by learn

--- db/migrate/20171106210827_create_table_users.rb
+++ db/migrate/20171106210827_create_table_users.rb
@@ -1,12 +1,13 @@
 class CreateTableUsers < ActiveRecord::Migration
   def up
+    drop_table :users
+    
+  end
+
+  def down
     create_table :users do |t|
       t.string :username
       t.string :password_digest
     end
-  end
-
-  def down
-    drop_table :users
   end
 end
